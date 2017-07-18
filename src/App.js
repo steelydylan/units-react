875bb5d4a46c577fab23f445c19855a3ecf2e413
steelydylan <ess_president@me.com>
2017-07-19 01:18:12 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div>
         <DocumentTheme 
           header={<Header title="Horic Design" items={[{label:'menu1',url:'#menu1'},{label:'menu2',url:'#menu2'}]} modifier="fixed"/>}
-          sidebar={<Sidebar items={[{label:'menu1',url:'#menu1'},{label:'menu2',url:'#menu2'}]} />}
+          sidebar={<Sidebar items={[{label:'menu'},{label:'menu1',url:'#menu1'},{label:'menu2',url:'#menu2',current:true}]} />}
           main={<Hero title="Horic Design" image="https://source.unsplash.com/9kbmpWBTk9A/1600x900/" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima assumenda facere iusto eligendi similique, saepe necessitatibus amet non consectetur, esse inventore, laboriosam ratione consequuntur ducimus. Inventore soluta et, quibusdam! Aperiam?"/>}
         />
       </div>
--- src/sidebar/index.js
+++ src/sidebar/index.js
@@ -4,7 +4,7 @@ import 'unitscss/dist/partial/sidebar.css';
 export default ({items}) => (
     <div className="uc-sidebar-wrap">
         <div className="uc-sidebar-list">
-            {items.map((item) => item.url ? <p><strong>label</strong></p> : <a href={item.url} className={item.current ? '_is-current' : ''}>{item.label}</a>)}
+            {items.map((item) => item.url ? <a href={item.url} className={item.current ? '_is-current' : ''}>{item.label}</a> : <p><strong>{item.label}</strong></p>)}
         </div>
     </div>
 );
